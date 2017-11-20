f3823a5e8b080e8c835e41cdc7c3a6c8888d41bc
ikeogu <ikeogu322@gmail.com>
2017-11-20 05:19:17 +0100

    login and registration page plus code

---
+++ login_reg.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>signup</title>
+
+        
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+
+       
+       
+    </head>
+
+    <body>
+
+        <!-- Top content -->
+        <div class="top-content">
+        	<div class="container">
+                	
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2 text">
+                        <h1> Registration Forms</h1>
+                        <div class="description">
+                        	<p>please kindly fill in the details below for proper working together.
+                        	</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1 show-forms">
+                    	<span class="show-register-form active">Register</span> 
+                    	<span class="show-forms-divider">/</span> 
+                    	<span class="show-login-form">Login</span>
+                    </div>
+                </div>
+                
+                <div class="row register-form">
+                    <div class="col-sm-4 col-sm-offset-1">
+						<form role="form" action="" method="post" class="r-form">
+	                    	<div class="form-group">
+	                    		<label class="sr-only" for="r-form-first-name">First name</label>
+	                        	<input type="text" name="firstName" placeholder="First name..." class="r-form-first-name form-control" id="r-form-first-name">
+	                        </div>
+	                        <div class="form-group">
+	                        	<label class="sr-only" for="r-form-last-name">Last name</label>
+	                        	<input type="text" name="r-form-last-name" placeholder="Last name..." class="r-form-last-name form-control" id="lastName">
+	                        </div>
+                            <div class="form-group">
+                                <label class="sr-only" for="r-form-email">Username</label>
+                                <input type="text" name="username" placeholder="Username..." class="r-form-email form-control" id="r-form-email">
+                            </div>
+	                        <div class="form-group">
+	                        	<label class="sr-only" for="r-form-email">Email</label>
+	                        	<input type="text" name="email" placeholder="Email..." class="r-form-email form-control" id="r-form-email">
+	                        </div>
+	                        <div class="form-group">
+	                        	<label class="sr-only" for="r-form-about-yourself">About yourself</label>
+	                        	<textarea name="r-form-about-yourself" placeholder="About yourself..." 
+	                        				class="r-form-about-yourself form-control" id="r-form-about-yourself"></textarea>
+	                        </div>
+				            <button type="submit" class="btn">Register !</button>
+						</form>
+                    </div>
+                    <div class="col-sm-6 forms-right-icons">
+						<div class="row">
+							<div class="col-sm-2 icon"><i class="fa fa-pencil"></i></div>
+							<div class="col-sm-10">
+								<h3>Depression </h3>
+                                <p>There are many emotional and psychological reactions that victims of rape and sexual assault can experience. One of the most common of these is depression.</p>
+								
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2 icon"><i class="fa fa-commenting"></i></div>
+							<div class="col-sm-10">
+								<h3>Adult Survivors of Child Sexual Abuse</h3>
+                            <p> Many perpetrators of sexual abuse are in a position of trust or responsible for the child’s care, such as a family member, teacher, clergy member, or coach.</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2 icon"><i class="fa fa-magic"></i></div>
+							<div class="col-sm-10">
+								<h3>Great Registration</h3>
+								<p>Please register for sponsorship, councelor, to help the vitim s of sexual or domestic assaults  and restore them to normal thinking, feeling and other things. please be in thier shoes and help save the world.</p>
+							</div>
+						</div>
+                    </div>
+                </div>
+                
+                <div class="row login-form">
+                    <div class="col-sm-4 col-sm-offset-1">
+						<form role="form" action="" method="post" class="l-form">
+	                    	<div class="form-group">
+	                    		<label class="sr-only" for="l-form-username">Username</label>
+	                        	<input type="text" name="username" placeholder="Username..." class="l-form-username form-control" id="l-form-username">
+	                        </div>
+	                        <div class="form-group">
+	                        	<label class="sr-only" for="l-form-password">Password</label>
+	                        	<input type="password" name="password" placeholder="Password..." class="l-form-password form-control" id="l-form-password">
+	                        </div>
+				            <button type="submit" class="btn">Sign in!</button>
+				    	</form>
+				    	<div class="social-login">
+                        	<p>Or login with:</p>
+                        	<div class="social-login-buttons">
+	                        	<a class="btn btn-link-1" href="#"><i class="fa fa-facebook"></i></a>
+	                        	<a class="btn btn-link-1" href="#"><i class="fa fa-twitter"></i></a>
+	                        	<a class="btn btn-link-1" href="#"><i class="fa fa-google-plus"></i></a>
+                        	</div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 forms-right-icons">
+						<div class="row">
+							<div class="col-sm-2 icon"><i class="fa fa-user"></i></div>
+							<div class="col-sm-10">
+								<h3>Thanks for registering</h3>
+								<p>Your lucrative assist will save and change most life which  were affected by assault.</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2 icon"><i class="fa fa-eye"></i></div>
+							<div class="col-sm-10">
+								<h3>Easy To Use</h3>
+								<p>Just click on the login button and see the people you can help either by your council,provision or advise. </p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2 icon"><i class="fa fa-twitter"></i></div>
+							<div class="col-sm-10">
+								<h3>Social Integrated</h3>
+								<p>You can also reach  to most victims either by email or contact.</p>
+							</div>
+						</div>
+                    </div>
+                </div>
+                    
+        	</div>
+        </div>
+
+        <!-- Footer -->
+        <footer>
+        	<div class="container">
+        		<div class="row">
+        			
+        			<div class="col-sm-8 col-sm-offset-2">
+        				<div class="footer-border"></div>
+                        <p>say NO 2  HUMAN ASSAULT</p>
+        				</div>
+        			
+        		</div>
+        	</div>
+        </footer>
+
+        <!-- Javascript -->
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+        
+       
+
+    </body>
+
+</html>
